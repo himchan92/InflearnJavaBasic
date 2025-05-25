c646a6fbc4e21adf7cf5ef55e233be23aec37a7e
omea92 <thdjun92@naver.com>
2025-05-25 21:50:38 +0900

    세션4. 절차지향 예제 실습3 - 메소드화

--- src/oop1/MusicPlayer2Main.java
+++ src/oop1/MusicPlayer2Main.java
@@ -5,18 +5,25 @@ public class MusicPlayer2Main {
     public static void main(String[] args) {
         MusicPlayerData data = new MusicPlayerData();
 
-        data.isOn = true;
-        System.out.println("음악 시작");
+        musicOn(data);
 
-        data.volume++;
-        System.out.println("증가" + data.volume);
+        volumeUp(data);
 
-        data.volume++;
-        System.out.println("증가" + data.volume);
+        volumeUp(data);
 
-        data.volume--;
-        System.out.println("감소" + data.volume);
+        volumeDown(data);
 
+        volumeChk(data);
+
+        musicOff(data);
+    }
+
+    private static void musicOff(MusicPlayerData data) {
+        data.isOn = false;
+        System.out.println("음악 종료");
+    }
+
+    private static void volumeChk(MusicPlayerData data) {
         System.out.println("음악 상태");
         if(data.isOn) {
             System.out.println("현재 " + data.volume);
@@ -24,8 +31,20 @@ public class MusicPlayer2Main {
         else {
             System.out.println("종료");
         }
+    }
 
-        data.isOn = false;
-        System.out.println("음악 종료");
+    private static void volumeDown(MusicPlayerData data) {
+        data.volume--;
+        System.out.println("감소" + data.volume);
+    }
+
+    private static void volumeUp(MusicPlayerData data) {
+        data.volume++;
+        System.out.println("증가" + data.volume);
+    }
+
+    private static void musicOn(MusicPlayerData data) {
+        data.isOn = true;
+        System.out.println("음악 시작");
     }
 }
